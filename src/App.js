7839ace36748d440eac971f5aebeefc05689bc22
ahmedelshkh <ahmedelshkh6566@gmail.com>
2025-07-27 16:14:04 +0300

    finished the header section and added the Browser Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderSec from './compontents/HeaderSec';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+
 
 function App() {
   return (
